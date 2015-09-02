898cdd774bd540c51569dc099461299ed0ac6789
郑树新 <zhengshuxin@gmail.com>
2015-09-02 20:44:46 +0800

    class smtp_client supports SSL send mail.

--- lib_acl_cpp/include/acl_cpp/smtp/smtp_client.hpp
+++ lib_acl_cpp/include/acl_cpp/smtp/smtp_client.hpp
@@ -9,33 +9,97 @@ namespace acl {
 
 class dbuf_pool;
 class istream;
+class polarssl_conf;
 
 class ACL_CPP_API smtp_client
 {
 public:
-	smtp_client(const char* addr, int conn_timeout,
-		int rw_timeout, bool use_ssl = false);
+	smtp_client(const char* addr, int conn_timeout, int rw_timeout);
 	~smtp_client();
 
+	smtp_client& set_ssl(polarssl_conf* ssl_conf);
 	smtp_client& set_auth(const char* user, const char* pass);
 	smtp_client& set_from(const char* from);
+	smtp_client& add_recipients(const char* recipients);
 	smtp_client& add_to(const char* to);
 
 	int get_smtp_code() const;
 	const char* get_smtp_status() const;
 
+	/**
+	 * 发送邮件信封过程，如果连接未打开，则自动打开连接，然后发送如下命令：
+	 * ehlo/helo, auth login, mail from, rcpt to, data
+	 * 该函数其实会调用下面的函数组合：open, get_banner, greet, auth_login,
+	 * mail_from, to_recipients
+	 * @return {bool}
+	 */
 	bool send_envelope();
 
+	/**
+	 * 开始发送邮件数据命令:DATA
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool data_begin();
+
+	/**
+	 * 发送邮件体数据，可以循环调用本函数，但数据内容必须是严格的邮件格式
+	 * @param data {const char*} 邮件内容
+	 * @param len {size_t} data 邮件数据长度
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool write(const char* data, size_t len);
+
+	/**
+	 * 发送邮件体数据，可以循环调用本函数，但数据内容必须是严格的邮件格式
+	 * @param fmt {const char*} 变参格式
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool format(const char* fmt, ...);
+
+	/**
+	 * 发送邮件体数据，可以循环调用本函数，但数据内容必须是严格的邮件格式
+	 * @param fmt {const char*} 变参格式
+	 * @param ap {va_list}
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool vformat(const char* fmt, va_list ap);
+
+	/**
+	 * 发送一封完整的邮件，需要给出邮件存储于磁盘上的路径
+	 * @param filepath {const char*} 邮件文件路径
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool send(const char* filepath);
+
+	/**
+	 * 发送一封完整的邮件，需要给出邮件文件的输入流对象
+	 * @param in {istream&} 邮件数据输入流
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool send(istream& in);
+
+	/**
+	 * 邮件发送完毕后，最后必须调用本函数告诉邮件服务器发送数据结束
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool data_end();
 
+	/**
+	 * 断开与邮件服务器的连接
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool quit();
+
+	/**
+	 * NOOP 命令
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool noop();
+
+	/**
+	 * 重置与邮件服务器的连接状态
+	 * @return {bool} 命令操作是否成功
+	 */
 	bool reset();
 
 	/////////////////////////////////////////////////////////////////////
@@ -48,7 +112,7 @@ public:
 	bool to_recipients();
 	
 private:
-	bool  use_ssl_;
+	polarssl_conf* ssl_conf_;
 	dbuf_pool* dbuf_;
 	char* addr_;
 	int   conn_timeout_;
--- lib_acl_cpp/src/smtp/smtp_client.cpp
+++ lib_acl_cpp/src/smtp/smtp_client.cpp
@@ -1,14 +1,15 @@
 #include "acl_stdafx.hpp"
 #include "acl_cpp/stdlib/log.hpp"
 #include "acl_cpp/stdlib/util.hpp"
-#include "acl_cpp/stream/istream.hpp"
 #include "acl_cpp/stdlib/dbuf_pool.hpp"
+#include "acl_cpp/stream/istream.hpp"
+#include "acl_cpp/stream/polarssl_conf.hpp"
+#include "acl_cpp/stream/polarssl_io.hpp"
 #include "acl_cpp/smtp/smtp_client.hpp"
 
 namespace acl {
 
-smtp_client::smtp_client(const char* addr, int conn_timeout,
-	int rw_timeout, bool use_ssl /* = false */)
+smtp_client::smtp_client(const char* addr, int conn_timeout, int rw_timeout)
 {
 	dbuf_ = new dbuf_pool;
 	addr_ = dbuf_->dbuf_strdup(addr);
@@ -20,15 +21,29 @@ smtp_client::smtp_client(const char* addr, int conn_timeout,
 	auth_user_ = NULL;
 	auth_pass_ = NULL;
 	from_ = NULL;
-	use_ssl_ = use_ssl;
+	ssl_conf_ = NULL;
 }
 
 smtp_client::~smtp_client()
 {
-	stream_.unbind();
 	if (client_)
+	{
+		// 将 SMTP_CLIENT 对象的流置空，以避免内部再次释放，
+		// 因为该流对象会在下面 stream_.close() 时被释放
+		client_->conn = NULL;
 		smtp_close(client_);
+	}
+
+	// 当 socket 流对象打开着，则关闭之，同时将依附于其的 SSL 对象释放
+	if (stream_.opened())
+		stream_.close();
 	dbuf_->destroy();
+}
+
+smtp_client& smtp_client::set_ssl(polarssl_conf* ssl_conf)
+{
+	ssl_conf_ = ssl_conf;
+	return *this;
 }
 
 smtp_client& smtp_client::set_auth(const char* user, const char* pass)
@@ -46,6 +61,17 @@ smtp_client& smtp_client::set_from(const char* from)
 {
 	if (from && *from)
 		from_ = dbuf_->dbuf_strdup(from);
+
+	return *this;
+}
+
+smtp_client& smtp_client::add_recipients(const char* recipients)
+{
+	string buf(recipients);
+	std::list<string>& tokens = buf.split(" \t;,");
+	std::list<string>::const_iterator cit;
+	for (cit = tokens.begin(); cit != tokens.end(); ++cit)
+		(void) add_to((*cit).c_str());
 
 	return *this;
 }
@@ -87,17 +113,32 @@ bool smtp_client::send_envelope()
 
 bool smtp_client::open()
 {
+	if (stream_.opened())
+	{
+		acl_assert(client_ != NULL);
+		acl_assert(client_->conn == stream_.get_vstream());
+		return true;
+	}
+
 	client_ = smtp_open(addr_, conn_timeout_, rw_timeout_, 1024);
 	if (client_ == NULL)
 	{
 		logger_error("connect %s error: %s", addr_, last_serror());
 		return false;
 	}
+
 	// 打开流对象
 	stream_.open(client_->conn);
-	if (use_ssl_)
-	{
 
+	if (ssl_conf_)
+	{
+		polarssl_io* ssl = new polarssl_io(*ssl_conf_, false);
+		if (stream_.setup_hook(ssl) == ssl)
+		{
+			logger_error("open ssl client error!");
+			ssl->destroy();
+			return false;
+		}
 	}
 	return true;
 }
