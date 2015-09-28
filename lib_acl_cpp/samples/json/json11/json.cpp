82f9c8088bb31f6bc44ea957561b7350990bd996
郑树新 <zhengshuxin@gmail.com>
2015-09-28 20:25:31 +0800

    no commit message

--- lib_acl_cpp/samples/json/json11/json.cpp
+++ lib_acl_cpp/samples/json/json11/json.cpp
@@ -1,16 +1,7 @@
 #include "stdafx.h"
 
-int main()
+static bool test(const char* sss)
 {
-	const char* sss =
-		"{\r\n"
-		"	\"data\" : \"dGVzdHFxcQ==\", \r\n"
-		"	\"receiver_id\" : [\r\n"
-		"		\"1442365683\"\r\n"
-		"	],\r\n"
-		"	\"extra\" : \"\",\r\n"
-		"	\"group_id\" : \"\"\r\n"
-		"}\r\n";
 	acl::json json;
 	const char* ptr = json.update(sss);
 
@@ -24,7 +15,7 @@ int main()
 		if (node == NULL)
 		{
 			printf("get_obj null error\r\n");
-			return 1;
+			return false;
 		}
 
 		printf("--------list all elements of the array --------\r\n");
@@ -54,5 +45,50 @@ int main()
 
 	printf(">>>to string: %s\r\n", json.to_string().c_str());
 
+	return true;
+}
+
+int main()
+{
+	const char* sss =
+		"{\r\n"
+		"	\"data\" : \"dGVzdHFxcQ==\", \r\n"
+		"	\"receiver_id\" : [\r\n"
+		"		\"1442365683\"\r\n"
+		"	],\r\n"
+		"	\"extra\" : \"\",\r\n"
+		"	\"group_id\" : \"\"\r\n"
+		"}\r\n";
+
+	if (test(sss) == false)
+		return 1;
+
+	printf("Enter any key to continue ...");
+	fflush(stdout);
+	getchar();
+
+	sss =
+		"{'forward': [\r\n"
+		"  {'url' : 'http://127.0.0.1/' },\r\n"
+		"  {\r\n"
+		"    'deny': [\r\n"
+		"         {\r\n"
+		"           'host': [\r\n"
+		"              'www.baidu.com',\r\n"
+		"              'www.sina.com'\r\n"
+		"           ]\r\n"
+		"         }\r\n"
+		"    ]\r\n"
+		"  },\r\n"
+		"  {\r\n"
+		"    'allow': [\r\n"
+		"         '111',\r\n"
+		"         '222'\r\n"
+		"      ]\r\n"
+		"  }\r\n"
+		"]}\r\n";
+
+	if (test(sss) == false)
+		return 1;
 	return 0;
 }
