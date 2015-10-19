fa31448ebba2b8d0810a7d864a93fddde914b3c1
郑树新 <zhengshuxin@gmail.com>
2015-10-19 23:02:50 +0800

    redis_client_cluster/redis_client_pool/redis_client support authtication with connection password

--- lib_acl_cpp/src/redis/redis_client_pool.cpp
+++ lib_acl_cpp/src/redis/redis_client_pool.cpp
@@ -10,6 +10,7 @@ redis_client_pool::redis_client_pool(const char* addr, size_t count,
 : connect_pool(addr, count, idx)
 , conn_timeout_(30)
 , rw_timeout_(60)
+, pass_(NULL)
 {
 }
 
@@ -25,10 +26,23 @@ redis_client_pool& redis_client_pool::set_timeout(int conn_timeout,
 	return *this;
 }
 
+redis_client_pool& redis_client_pool::set_password(const char* pass)
+{
+	if (pass_)
+		acl_myfree(pass_);
+	if (pass && *pass)
+		pass_ = acl_mystrdup(pass);
+	else
+		pass_ = NULL;
+	return *this;
+}
+
 connect_client* redis_client_pool::create_connect()
 {
 	redis_client* conn = NEW redis_client(addr_, conn_timeout_,
 		rw_timeout_);
+	if (pass_)
+		conn->set_password(pass_);
 	return conn;
 }
 
