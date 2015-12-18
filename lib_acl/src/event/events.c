9a477daaac36d8a191fb0580ef71875f2c87c8ca
郑树新 <zhengshuxin@gmail.com>
2015-12-18 16:30:30 +0800

    no commit message

--- lib_acl/src/event/events.c
+++ lib_acl/src/event/events.c
@@ -26,7 +26,7 @@ void event_check_fds(ACL_EVENT *ev)
 				fdp->fdidx_ready = ev->fdcnt_ready;
 				ev->fdtabs_ready[ev->fdcnt_ready++] = fdp;
 			} else if (fdp->stream->sys_read_ready
-				&& !(fdp->event_type & ACL_EVENT_ACCEPT))
+				&& !fdp->listener)
 			{
 				fdp->event_type |= ACL_EVENT_READ;
 				fdp->fdidx_ready = ev->fdcnt_ready;
@@ -72,7 +72,7 @@ int event_prepare(ACL_EVENT *ev)
 				fdp->fdidx_ready = ev->fdcnt_ready;
 				ev->fdtabs_ready[ev->fdcnt_ready++] = fdp;
 			} else if (fdp->stream->sys_read_ready
-				&& !(fdp->event_type & ACL_EVENT_ACCEPT))
+				&& !fdp->listener)
 			{
 				fdp->event_type |= ACL_EVENT_READ;
 				fdp->fdidx_ready = ev->fdcnt_ready;
@@ -207,7 +207,7 @@ int event_thr_prepare(ACL_EVENT *ev)
 				fdp->fdidx_ready = ev->fdcnt_ready;
 				ev->fdtabs_ready[ev->fdcnt_ready++] = fdp;
 			} else if (fdp->stream->sys_read_ready
-				&& !(fdp->event_type & ACL_EVENT_ACCEPT))
+				&& !fdp->listener)
 			{
 				fdp->event_type = ACL_EVENT_READ;
 				fdp->fdidx_ready = ev->fdcnt_ready;
