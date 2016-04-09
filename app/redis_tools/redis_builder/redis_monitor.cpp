0200ee0198a4c946bf83de51931904b99a3eae53
郑树新 <zhengshuxin@gmail.com>
2016-04-08 20:57:45 -0400

    no commit message

--- app/redis_tools/redis_builder/redis_monitor.cpp
+++ app/redis_tools/redis_builder/redis_monitor.cpp
@@ -194,15 +194,15 @@ void redis_monitor::show_status(std::vector<acl::redis_client*>& conns)
 
 #ifdef ACL_UNIX
 		printf("\033[1;32;40mredis:\033[0m"
-			" \033[1;36;40m%s\033[0m,"
+			" \033[1;36;40m%20s\033[0m,"
 			" \033[1;33;40mtps:\033[0m"
-			" \033[1;36;40m%d\033[0m,"
+			" \033[1;36;40m%8d\033[0m,"
 			" \033[1;33;40mclients:\033[0m"
-			" \033[1;36;40m%d\033[0m,"
+			" \033[1;36;40m%8d\033[0m,"
 			" \033[1;33;40mkeys:\033[0m"
-			" \033[1;36;40m%d\033[0m,"
+			" \033[1;36;40m%8d\033[0m,"
 			" \033[1;33;40mmemory:\033[0m"
-			" \033[1;36;40m%.2f %s\033[0m\r\n",
+			" \033[1;36;40m%8.2f %s\033[0m\r\n",
 			addrs[i].c_str(), tpses[i], clients[i], keys[i],
 			msize, units);
 #else
@@ -235,15 +235,15 @@ void redis_monitor::show_status(std::vector<acl::redis_client*>& conns)
 
 #ifdef ACL_UNIX
 	printf("\033[1;34;40mtotal masters:\033[0m"
-		" \033[1;36;40m%d\033[0m,"
-		" \033[1;34;40mtotal tps:\033[0m"
-		" \033[1;36;40m%d\033[0m,"
-		" \033[1;34;40mtotal client:\033[0m"
-		" \033[1;36;40m%d\033[0m,"
-		" \033[1;34;40mtotal keys:\033[0m"
-		" \033[1;36;40m%d\033[0m,"
-		" \033[1;34;40mtotal memory:\033[0m"
-		" \033[1;36;40m%.2f %s\033[0m\r\n",
+		" \033[1;36;40m%12d\033[0m,"
+		" \033[1;34;40mtps:\033[0m"
+		" \033[1;36;40m%8d\033[0m,"
+		" \033[1;34;40mclients:\033[0m"
+		" \033[1;36;40m%8d\033[0m,"
+		" \033[1;34;40mkeys:\033[0m"
+		" \033[1;36;40m%8d\033[0m,"
+		" \033[1;34;40mmemory:\033[0m"
+		" \033[1;36;40m%8.2f %s\033[0m\r\n",
 		(int) size, all_tps, all_client, all_keys, msize, units);
 #else
 	printf("total masters: %d, total tps: %d, total clients: %d,"
