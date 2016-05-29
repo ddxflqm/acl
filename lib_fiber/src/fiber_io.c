9ae2959fd616a1e11eab1e2f23f1942580feceda
郑树新 <zhengshuxin@gmail.com>
2016-05-29 21:57:54 +0800

    no commit message

---
+++ lib_fiber/src/fiber_io.c
@@ -0,0 +1,168 @@
+#include "stdafx.h"
+#include <fcntl.h>
+#define __USE_GNU
+#include <dlfcn.h>
+#include <sys/stat.h>
+#include "event.h"
+#include "fiber_schedule.h"
+#include "fiber_io.h"
+
+typedef ssize_t (*read_fn)(int, void *, size_t);
+typedef ssize_t (*write_fn)(int, const void *, size_t);
+typedef int (*accept_fn)(int, struct sockaddr *, socklen_t *);
+
+static read_fn   __sys_read   = NULL;
+static write_fn  __sys_write  = NULL;
+static accept_fn __sys_accept = NULL;
+static EVENT    *__event      = NULL;
+static FIBER   **__io_fibers  = NULL;
+static size_t    __io_count   = 0;
+
+static void fiber_io_loop(void *ctx);
+
+#ifdef ACL_UNIX
+void fiber_io_hook(void) __attribute__ ((constructor));
+#endif
+
+#define MAXFD	1024
+
+void fiber_io_hook(void)
+{
+	__sys_read   = (read_fn) dlsym(RTLD_NEXT, "read");
+	__sys_write  = (write_fn) dlsym(RTLD_NEXT, "write");
+	__sys_accept = (accept_fn) dlsym(RTLD_NEXT, "accept");
+
+	__event = event_create(MAXFD);
+	__io_fibers = (FIBER **) acl_mycalloc(1, sizeof(FIBER *));
+
+	(void) fiber_create(fiber_io_loop, __event, 32768);
+}
+
+static void fiber_io_loop(void *ctx)
+{
+	EVENT *ev = (EVENT *) ctx;
+
+	fiber_system();
+
+	for (;;) {
+		while (fiber_yield() > 0) {
+			// do nothing;
+		}
+
+		event_loop(ev);
+	}
+}
+
+static void accept_callback(EVENT *ev, int fd, void *ctx acl_unused, int mask)
+{
+	event_del(ev, fd, mask);
+
+	fiber_ready(__io_fibers[fd]);
+	__io_count--;
+	__io_fibers[fd] = __io_fibers[__io_count];
+}
+
+
+static void fiber_wait_accept(int fd)
+{
+	event_add(__event, fd, EVENT_READABLE, accept_callback, NULL);
+
+	__io_fibers[fd] = fiber_running();
+	__io_count++;
+	fiber_switch();
+}
+
+int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
+{
+	while (1) {
+		int clifd = __sys_accept(sockfd, addr, addrlen);
+		if (clifd >= 0) {
+			acl_non_blocking(clifd, ACL_NON_BLOCKING);
+			acl_tcp_nodelay(clifd, 1);
+			return clifd;
+		}
+
+#if EAGAIN == EWOULDBLOCK
+		if (errno != EAGAIN)
+#else
+		if (errno != EAGAIN && errno != EWOULDBLOCK)
+#endif
+			return -1;
+
+		fiber_wait_accept(sockfd);
+	}
+}
+
+static void read_callback(EVENT *ev, int fd, void *ctx acl_unused, int mask)
+{
+	event_del(ev, fd, mask);
+
+	fiber_ready(__io_fibers[fd]);
+	__io_count--;
+	__io_fibers[fd] = __io_fibers[__io_count];
+}
+
+static void fiber_wait_read(int fd)
+{
+	event_add(__event, fd, EVENT_READABLE, read_callback, NULL);
+
+	__io_fibers[fd] = fiber_running();
+	__io_count++;
+	fiber_switch();
+}
+
+ssize_t read(int fd, void *buf, size_t count)
+{
+	while (1) {
+		ssize_t n = __sys_read(fd, buf, count);
+
+		if (n >= 0)
+			return n;
+
+#if EAGAIN == EWOULDBLOCK
+		if (errno != EAGAIN)
+#else
+		if (errno != EAGAIN && errno != EWOULDBLOCK)
+#endif
+			return -1;
+
+		fiber_wait_read(fd);
+	}
+}
+
+static void write_callback(EVENT *ev, int fd, void *ctx acl_unused, int mask)
+{
+	event_del(ev, fd, mask);
+
+	fiber_ready(__io_fibers[fd]);
+	__io_count--;
+	__io_fibers[fd] = __io_fibers[__io_count];
+}
+
+static void fiber_wait_write(int fd)
+{
+	event_add(__event, fd, EVENT_WRITABLE, write_callback, NULL);
+
+	__io_fibers[fd] = fiber_running();
+	__io_count++;
+	fiber_switch();
+}
+
+ssize_t write(int fd, const void *buf, size_t count)
+{
+	while (1) {
+		ssize_t n = __sys_write(fd, buf, count);
+
+		if (n >= 0)
+			return n;
+
+#if EAGAIN == EWOULDBLOCK
+		if (errno != EAGAIN)
+#else
+		if (errno != EAGAIN && errno != EWOULDBLOCK)
+#endif
+			return -1;
+
+		fiber_wait_write(fd);
+	}
+}
---
+++ lib_fiber/src/fiber_schedule.c
@@ -0,0 +1,176 @@
+#include "stdafx.h"
+#include "fiber_schedule.h"
+
+static ACL_RING __fibers_queue;
+static FIBER  **__fibers = NULL;
+static size_t   __fibers_size = 0;
+static int      __fiber_exitcode = 0;
+static FIBER   *__fiber_running = NULL;
+static FIBER    __fiber_schedule;
+static size_t   __fiber_idgen = 0;
+static int      __fiber_count = 0;
+static int      __fiber_switched = 0;
+
+static void fiber_swap(FIBER *from, FIBER *to)
+{
+	if (swapcontext(&from->uctx, &to->uctx) < 0)
+		acl_msg_fatal("%s(%d), %s: swapcontext error %s",
+			__FILE__, __LINE__, __FUNCTION__, acl_last_serror());
+}
+
+FIBER *fiber_running(void)
+{
+	return __fiber_running;
+}
+
+void fiber_exit(int exit_code)
+{
+	__fiber_exitcode = exit_code;
+	__fiber_running->status = FIBER_STATUS_EXITING;
+	fiber_switch();
+}
+
+static void fiber_start(unsigned int x, unsigned int y)
+{
+	FIBER *fiber;
+	unsigned long z;
+
+	z = x << 16;
+	z <<= 16;
+	z |= y;
+	fiber = (FIBER *) z;
+
+	fiber->fun(fiber->arg);
+	fiber_exit(0);
+}
+
+static FIBER *fiber_alloc(void (*fun)(void *), void *arg, size_t size)
+{
+	FIBER *fiber = (FIBER *) acl_mycalloc(1, sizeof(FIBER) + size);
+	sigset_t zero;
+	unsigned long z;
+	unsigned int x, y;
+
+	fiber->fun   = fun;
+	fiber->arg   = arg;
+	fiber->stack = fiber->buf;
+	fiber->size  = size;
+	fiber->id    = ++__fiber_idgen;
+
+	sigemptyset(&zero);
+	sigprocmask(SIG_BLOCK, &zero, &fiber->uctx.uc_sigmask);
+
+	if (getcontext(&fiber->uctx) < 0)
+		acl_msg_fatal("%s(%d), %s: getcontext error: %s",
+			__FILE__, __LINE__, __FUNCTION__, acl_last_serror());
+
+	fiber->uctx.uc_stack.ss_sp   = fiber->stack + 8;
+	fiber->uctx.uc_stack.ss_size = fiber->size - 64;
+
+	z = (unsigned long) fiber;
+	y = z;
+	z >>= 16;
+	x = z >> 16;
+
+	makecontext(&fiber->uctx, (void(*)(void)) fiber_start, 2, x, y);
+
+	return fiber;
+}
+
+FIBER *fiber_create(void (*fun)(void *), void *arg, size_t size)
+{
+	FIBER *fiber = fiber_alloc(fun, arg, size);
+
+	__fiber_count++;
+	if (__fibers_size % 64 == 0)
+		__fibers = (FIBER **) acl_myrealloc(__fibers, 
+				(__fibers_size + 64) * sizeof(FIBER *));
+
+	fiber->slot = __fibers_size;
+	__fibers[__fibers_size++] = fiber;
+	fiber_ready(fiber);
+
+	return fiber;
+}
+
+void fiber_free(FIBER *fiber)
+{
+	acl_myfree(fiber);
+}
+
+#ifdef ACL_UNIX
+void fiber_init(void) __attribute__ ((constructor));
+#endif
+
+void fiber_init(void)
+{
+	acl_ring_init(&__fibers_queue);
+}
+
+void fiber_schedule(void)
+{
+	FIBER *fiber;
+	ACL_RING *head;
+
+	for (;;) {
+		head = acl_ring_pop_head(&__fibers_queue);
+		if (head == NULL) {
+			printf("no fiber now\r\n");
+			break;
+		}
+
+		fiber = ACL_RING_TO_APPL(head, FIBER, me);
+		fiber->status = FIBER_STATUS_READY;
+
+		__fiber_running = fiber;
+		__fiber_switched++;
+
+		fiber_swap(&__fiber_schedule, fiber);
+		__fiber_running = NULL;
+
+		if (fiber->status == FIBER_STATUS_EXITING) {
+			size_t slot = fiber->slot;
+
+			if (!fiber->sys)
+				__fiber_count--;
+
+			__fibers[slot] = __fibers[--__fibers_size];
+			__fibers[slot]->slot = slot;
+			fiber_free(fiber);
+		}
+	}
+}
+
+static void fiber_add(FIBER *fiber)
+{
+	acl_ring_prepend(&__fibers_queue, &fiber->me);
+}
+
+void fiber_ready(FIBER *fiber)
+{
+	fiber->status = FIBER_STATUS_READY;
+	fiber_add(fiber);
+}
+
+int fiber_yield(void)
+{
+	int  n = __fiber_switched;
+
+	fiber_ready(__fiber_running);
+	fiber_switch();
+
+	return __fiber_switched - n - 1;
+}
+
+void fiber_system(void)
+{
+	if (!__fiber_running->sys) {
+		__fiber_running->sys = 1;
+		__fiber_count--;
+	}
+}
+
+void fiber_switch(void)
+{
+	fiber_swap(__fiber_running, &__fiber_schedule);
+}
