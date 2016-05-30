2e52f6f8ed8b5bd59cfca4b2a625afd24e7443fc
郑树新 <zhengshuxin@gmail.com>
2016-05-31 00:31:55 +0800

    no commit message

---
+++ lib_fiber/samples/httpd/main.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "http_servlet.h"
+
+static void http_server(void *ctx)
+{
+	acl::socket_stream *conn = (acl::socket_stream *) ctx;
+
+	acl::memcache_session session("127.0.0.1:11211");
+	http_servlet servlet(conn, &session);
+	servlet.setLocalCharset("gb2312");
+
+	while (true)
+	{
+		if (servlet.doRun() == false)
+			break;
+	}
+
+	delete conn;
+}
+
+static void fiber_accept(void *)
+{
+	acl::string addr = "127.0.0.1:9001";
+	acl::server_socket server;
+
+	if (server.open(addr) == false)
+	{
+		printf("open %s error\r\n", addr.c_str());
+		exit (1);
+	}
+	else
+		printf("open %s ok\r\n", addr.c_str());
+
+	while (true)
+	{
+		acl::socket_stream* client = server.accept();
+		if (client == NULL)
+		{
+			printf("accept failed: %s\r\n", acl::last_serror());
+			break;
+		}
+
+		fiber_create(http_server, client, 326780);
+	}
+
+	exit (0);
+}
+
+int main(void)
+{
+	acl::acl_cpp_init();
+	fiber_init();
+	fiber_create(fiber_accept, NULL, 32768);
+	fiber_schedule();
+}
