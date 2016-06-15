96bb26b17e56a80708c53aad1aa032541f10a21e
郑树新 <zhengshuxin@gmail.com>
2016-06-15 22:01:24 +0800

    no commit message

--- lib_fiber/include/fiber/lib_fiber.h
+++ lib_fiber/include/fiber/lib_fiber.h
@@ -1,7 +1,33 @@
-#ifndef ACL_FIBER_INCLUDE_H
-#define ACL_FIBER_INCLUDE_H
+#ifndef LIB_FIBER_INCLUDE_H
+#define LIB_FIBER_INCLUDE_H
 
-#include "fiber_schedule.h"
-#include "fiber_io.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct FIBER FIBER;
+
+FIBER *fiber_create(void (*fn)(FIBER *, void *), void *arg, size_t size);
+int fiber_id(const FIBER *fiber);
+void fiber_set_errno(FIBER *fiber, int errnum);
+int fiber_errno(FIBER *fiber);
+int fiber_status(const FIBER *fiber);
+int fiber_yield(void);
+void fiber_ready(FIBER *fiber);
+void fiber_switch(void);
+void fiber_schedule(void);
+
+void fiber_io_stop(void);
+unsigned int fiber_delay(unsigned int milliseconds);
+unsigned int fiber_sleep(unsigned int seconds);
+FIBER *fiber_create_timer(unsigned int milliseconds,
+	void (*fn)(FIBER *, void *), void *ctx);
+void fiber_reset_timer(FIBER *timer, unsigned int milliseconds);
+
+void fiber_set_dns(const char* ip, int port);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
