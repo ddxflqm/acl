10aba2309bcf810a7f53d1d9ecd086a190c16d9a
郑树新 <zhengshuxin@gmail.com>
2016-07-09 00:47:10 +0800

    no commit message

--- lib_fiber/c/include/fiber/lib_fiber.h
+++ lib_fiber/c/include/fiber/lib_fiber.h
@@ -241,6 +241,44 @@ void acl_fiber_rwlock_runlock(ACL_FIBER_RWLOCK *l);
  */
 void acl_fiber_rwlock_wunlock(ACL_FIBER_RWLOCK *l);
 
+/* fiber semaphore */
+
+typedef struct ACL_FIBER_SEM ACL_FIBER_SEM;
+
+/**
+ * 创建协程信号量
+ * @param max {int} 信号量最大值（必须 > 0）
+ * @return {ACL_FIBER_SEM *}
+ */
+ACL_FIBER_SEM *acl_fiber_sem_create(int max);
+
+/**
+ * 释放协程信号量
+ * @param {ACL_FIBER_SEM *}
+ */
+void acl_fiber_sem_free(ACL_FIBER_SEM *sem);
+
+/**
+ * 当协程信号量 > 0 时使信号量减 1，否则等待信号量 > 0
+ * @param sem {ACL_FIBER_SEM *}
+ * @retur {int} 返回信号量当前值
+ */
+int acl_fiber_sem_wait(ACL_FIBER_SEM *sem);
+
+/**
+ * 尝试使协程信号量减 1
+ * @param sem {ACL_FIBER_SEM *}
+ * @retur {int} 成功减 1 时返回值 >= 0，返回 -1 表示当前信号量不可用
+ */
+int acl_fiber_sem_trywait(ACL_FIBER_SEM *sem);
+
+/**
+ * 使协程信号量加 1
+ * @param sem {ACL_FIBER_SEM *}
+ * @retur {int} 返回信号量当前值
+ */
+int acl_fiber_sem_post(ACL_FIBER_SEM *sem);
+
 /* channel communication */
 
 /**
