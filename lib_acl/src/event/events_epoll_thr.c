ff9d66f3f44777a4fb6ff91ae864d3c4d871d772
郑树新 <zhengshuxin@gmail.com>
2016-08-11 13:18:45 +0800

    no commit message

--- lib_acl/src/event/events_epoll_thr.c
+++ lib_acl/src/event/events_epoll_thr.c
@@ -137,8 +137,6 @@ static void event_enable_read(ACL_EVENT *eventp, ACL_VSTREAM *fp,
 		return;
 	}
 
-	THREAD_UNLOCK(&evthr->event.tb_mutex);
-
 	if (epoll_ctl(evthr->handle, EPOLL_CTL_ADD, fd, &ev) < 0) {
 		if (errno == EEXIST)
 			acl_msg_warn("%s: epoll_ctl: %s, fd: %d",
@@ -152,6 +150,8 @@ static void event_enable_read(ACL_EVENT *eventp, ACL_VSTREAM *fp,
 			acl_msg_fatal("%s: epoll_ctl: %s, fd: %d, epfd: %d",
 				myname, acl_last_serror(), fd, evthr->handle);
 	}
+
+	THREAD_UNLOCK(&evthr->event.tb_mutex);
 }
 
 static void event_enable_listen(ACL_EVENT *eventp, ACL_VSTREAM *fp,
@@ -299,8 +299,6 @@ static void event_enable_write(ACL_EVENT *eventp, ACL_VSTREAM *fp,
 		THREAD_UNLOCK(&evthr->event.tb_mutex);
 		return;
 	}
-
-	THREAD_UNLOCK(&evthr->event.tb_mutex);
 	
 	if (epoll_ctl(evthr->handle, EPOLL_CTL_ADD, fd, &ev) < 0) {
 		if (errno == EEXIST)
@@ -315,6 +313,8 @@ static void event_enable_write(ACL_EVENT *eventp, ACL_VSTREAM *fp,
 			acl_msg_fatal("%s: epoll_ctl: %s, fd: %d, epfd: %d",
 				myname, acl_last_serror(), fd, evthr->handle);
 	}
+
+	THREAD_UNLOCK(&evthr->event.tb_mutex);
 }
 
 /* event_disable_readwrite - disable request for read or write events */
