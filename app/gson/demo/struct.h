a98bee7538861b7e155584623b713a06d04cdd92
郑树新 <zhengshuxin@gmail.com>
2016-10-08 23:18:57 +0800

    no commit message

--- app/gson/demo/struct.h
+++ app/gson/demo/struct.h
@@ -25,10 +25,47 @@ struct base
 	float *h_ptr;
 	double i;
 	double *i_ptr;
+
+	base()
+	{
+		a_ptr = NULL;
+		b_ptr = NULL;
+		c_ptr = NULL;
+		d_ptr = NULL;
+		e_ptr = NULL;
+		f_ptr = NULL;
+		g_ptr = NULL;
+		acl_string_ptr = NULL;
+	}
+
+	~base()
+	{
+		delete a_ptr;
+		delete b_ptr;
+		delete c_ptr;
+		delete d_ptr;
+		delete e_ptr;
+		delete f_ptr;
+		delete acl_string_ptr;
+	}
 };
 
 struct list1
 {
+	list1()
+	{
+		b_ptr = NULL;
+		bases_list_ptr = NULL;
+		bases_ptr_list_ptr = NULL;
+	}
+
+	~list1()
+	{
+		delete b_ptr;
+		delete bases_list_ptr;
+		delete bases_ptr_list_ptr;
+	}
+
 	base b;
 	base *b_ptr;
 	std::list<base> bases_list;
