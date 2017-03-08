4da2fc7675dd236225c40f32226dcf8066635e7e
zsx <zsx@zsxxsz.local>
2017-03-08 15:47:34 +0800

    migrating to xcode

--- lib_acl_cpp/src/mime/mime_node.cpp
+++ lib_acl_cpp/src/mime/mime_node.cpp
@@ -206,7 +206,7 @@ bool mime_node::save(pipe_manager& out, const char* src, int len) const
 	else
 		mime_decoder = NULL;
 
-	size_t n = m_bodyEnd - m_bodyBegin;
+	size_t n = (size_t) (m_bodyEnd - m_bodyBegin);
 	if (out.update(src + m_bodyBegin, n) == false)
 	{
 		delete mime_decoder;
