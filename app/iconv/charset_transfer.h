145a480e0ed1c313bc0d5f3745ed30024947a461
ubuntu14 <zsx@ubuntu>
2017-03-10 09:00:30 +0800

    add iconv app in acl for charset convert.

---
+++ app/iconv/charset_transfer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class charset_transfer
+{
+public:
+	charset_transfer(void) : utf8_bom_(false) {}
+	~charset_transfer(void) {}
+
+	charset_transfer& set_from_charset(const char* charset);
+	charset_transfer& set_to_charset(const char* charset);
+	charset_transfer& set_from_path(const char* path);
+	charset_transfer& set_to_path(const char* path);
+	charset_transfer& set_utf8bom(bool yes);
+
+	int run(bool recursive = true);
+
+private:
+	acl::string from_charset_;
+	acl::string to_charset_;
+	acl::string from_path_;
+	acl::string to_path_;
+	bool utf8_bom_;
+
+	bool check(void);
+	bool get_filepath(acl::scan_dir& scan, const char* filename,
+		acl::string& from_filepath, acl::string& to_filepath,
+		acl::string& to_path);
+	bool transfer(const char* from_file, const char* to_file);
+};
